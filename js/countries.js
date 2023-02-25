f0177070e0d2a4109c38b1b08b318f2ac43735af
Naimur Rahman <naimur2231@gmail.com>
2023-02-26 01:23:42 +0600

    dynamically data loded countries rest api and advance function

--- js/countries.js
+++ js/countries.js
@@ -9,13 +9,34 @@ const countryDisplay = countries => {
     // console.log(countries)
     countries.forEach(country => {
         const countryDiv = document.createElement('div');
-        console.log(country.capital)
+        console.log(country.cca2)
+        countryDiv.classList.add('country');
         countryDiv.innerHTML = `
            <h4>Country Name: ${country.name.common} </h4>
            <h6>Capital Name: ${country.capital ? country.capital[0] : 'No Capital'} </h4>
+           <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `;
         countryContainer.appendChild(countryDiv);
     });
+}
+
+const loadCountryDetails = code => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => countriesDetailDisplay(data[0]))
+
+    // console.log(url)
+}
+
+const countriesDetailDisplay = country => {
+    // console.log(country)
+    const countryDetails = document.getElementById('countries-details');
+    countryDetails.innerHTML = `
+      <h4>name: ${country.name.common} </h4>
+      <img src = "${country.flags.png}">
+    `
 }
 
 countryLoad();
