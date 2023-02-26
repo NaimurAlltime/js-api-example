3a88a3c701b3905887dc594fcbc314291a8ec026
Naimur Rahman <naimur2231@gmail.com>
2023-02-26 17:37:10 +0600

    Display dynamic search result and details show in modal

--- js/meal.js
+++ js/meal.js
@@ -22,6 +22,10 @@ const displayMeals = meals => {
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <!-- Button trigger modal -->
+            <button onclick="loadMealDetails2(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+            Details
+            </button>
             </div>
          </div>
         `;
@@ -35,6 +39,40 @@ const searchMeals = () => {
     const mealsText = document.getElementById('meals-text').value;
     // console.log(mealsText);
     loadMeals(mealsText);
+}
+
+const loadMealDetails = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]))
+    // error handling 
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+// or 
+// async await method
+const loadMealDetails2 = async(idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+   try {
+     const res = await fetch(url);
+     const data = await res.json();
+     displayMealDetails(data.meals[0]);
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+const displayMealDetails = meal => {
+    // meal title show the modal 
+   document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+   // meal img show the modal 
+   document.getElementById('mealDetailsBody').innerHTML = `
+    <img src="${meal.strMealThumb}" class="img-fluid">
+   `
 }
 
 loadMeals('fish');
